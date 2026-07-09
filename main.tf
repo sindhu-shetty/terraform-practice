39bbba2933249479beb207aff2d71f276d9784a9
sindhu-shetty <shettysindhu1997@gmail.com>
2026-07-09 10:46:24 +0530

    Added remote state with S3 backend

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "sindhu-terraform-state"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 provider  "aws"{
